84410778b783ef6bb18a4ac8e4181169b5196a6f
Harits <mharitsnf@gmail.com>
2022-10-19 14:53:33 +0200

    Implement test communication

--- src/websocket.js
+++ src/websocket.js
@@ -6,7 +6,11 @@ const createWebSocketServer = (server) => {
     wss = new websocket.Server({ server })
 
     wss.on("connection", ws => {
-        ws.on("message", message => console.log(message))
+        ws.on("message", (message, isBinary) => {
+            let msg = isBinary ? message : JSON.parse(message.toString())
+            console.log(msg)
+            ws.send(JSON.stringify({ message: "hello" }))
+        })
     })
     
     return wss
